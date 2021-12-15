f95aa9b8868a3d44c7d04dec8b5a8f9742fd9ac0
caueeugenio <caue_eugenio@hotmail.com>
2021-12-14 22:28:17 -0300

    14-12-2021: criacao filtro de imagem

--- src/app/components/buy-a-travel/buy-a-travel.component.ts
+++ src/app/components/buy-a-travel/buy-a-travel.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { THROW_IF_NOT_FOUND } from "@angular/core/src/di/injector";
 import { ActivatedRoute, Router } from "@angular/router";
+import { pipe } from "rxjs";
 import { filter, switchMap } from "rxjs/operators";
 import { Destinations } from "src/app/model/destinations";
 import { MainService } from "src/app/services/main.service";
@@ -13,26 +15,36 @@ export class BuyATravelComponent implements OnInit {
   destinations: Destinations[];
   id: any;
   name: any;
+  imgUrl: any;
+  price:any;
   constructor(
     private activatedRoute: ActivatedRoute,
     private mainService: MainService
   ) {}
 
   ngOnInit() {
-    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
     this.name = this.activatedRoute.snapshot.paramMap.get("cidade");
-    this.filterImage();
+    // this.filterImage();
+    this.getDestinations();
   }
 
-  filterImage() {
+  getDestinations() {
     this.mainService.getDestinations().subscribe(
       (data) => {
-     this.destinations = data;
-     filter( (url) =>P)
-    },
-    (error) =>{
-      console
-    }
+        this.destinations = data;
+        this.destinations.forEach((element) => {
+          if (element.id === this.id) {
+            this.imgUrl = element.url;
+            this.price = element.price;
+          }
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
     );
   }
+
+
 }
